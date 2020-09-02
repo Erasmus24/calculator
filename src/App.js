9d43dfef0ff21209d38065de08f9a521a6d0d4b4
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-09-02 03:02:29 +0200

    installation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is the React Calculator!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
